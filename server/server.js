f576a4a1b044a48fe45e522233e7be99f2885ca1
Liuminosity <liu.stanford@gmail.com>
2015-11-07 11:49:25 -0800

    [feat] (client): set up getData api endpoint on server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client'));
 
+app.use(bodyParser.json());
+
+//test url
 app.get('/', function(req, res) {
   res.send('hello world');
 });
+
+app.post('/getData', function(req, res) {
+  console.log(req.body);
+  res.send('sup');
+})
 
 app.listen(process.env.PORT || 3000);
