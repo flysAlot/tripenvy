a7dbf34cb6e8a378b493a316a97aff785fd2162b
Max Li <maximilianm.li@gmail.com>
2015-11-08 10:23:31 -0800

    add captions

--- client/src/components/MapModal.jsx
+++ client/src/components/MapModal.jsx
@@ -22,10 +22,6 @@ var MapModal = React.createClass({
         clearTravelPlan={this.props.clearTravelPlan}/></h1>;
   },
 
-  CaptionBlock: function CaptionBlock() {
-    return this.props.caption.length < 44 ? <span style={{'color': 'white', 'font': '13px/24px Monaco, Sans-Serif', 'letterSpacing': '-1px', 'background': 'rgb(0, 0, 0)', 'background': 'rgba(0, 0, 0, 0.7)', 'padding': '10px'}}>{this.props.caption}</span> :
-      <span style={{'color': 'white', 'font': '13px/24px Monaco, Sans-Serif', 'letterSpacing': '-1px', 'background': 'rgb(0, 0, 0)', 'background': 'rgba(0, 0, 0, 0.7)', 'padding': '10px'}}>{this.props.caption.slice(0,41)}...</span>;
-  },
 
   render: function render() {
     var city = this.props.cities[this.props.selectedCityIndex];
@@ -33,14 +29,12 @@ var MapModal = React.createClass({
     if (city) {
       for (var i = 0; i < Math.min(3, city.images.length); i++) {
         var imageUrl = city.images[i].images.low_resolution.url;
-        var caption = city.images[i].caption.text;
+        var caption = city.images[i].caption.text.slice(0,40);
         // console.log('this is imageUrl', imageUrl);
         imageArray.push(
-          <div>
           <div onClick={this.toggleActivities} className="img-box" key={i}>
             <img className="instagram-img" src={imageUrl} />
-          </div> 
-          <h3 style={{'position': 'absolute', 'top': '250px', 'left': '0', 'width': '100%'}}> 
+          <h3 style={{'top': '250px', 'textAlign':'center', 'width': '100%'}}> 
             <span style={{'color': 'white', 'font': '13px/24px Monaco, Sans-Serif', 'letterSpacing': '-1px', 'background': 'rgb(0, 0, 0)', 'background': 'rgba(0, 0, 0, 0.7)', 'padding': '10px'}}>{caption}</span>
           </h3>
           </div>
